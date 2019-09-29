3a945a73bc7b77ebc7134ae009400b62f2c7d8ca
gianlopes <54591501+gianlopes@users.noreply.github.com>
2019-09-29 18:30:35 -0300

    criei a classe literal

--- main.cpp
+++ main.cpp
@@ -3,6 +3,29 @@
 //Interface Exp
 class Exp {
 public:
-    virtual void print() = 0;
-    virtual int eval() = 0;
+	virtual void print() const = 0;
+	virtual int eval() const = 0;
 };
+
+class Literal: public Exp {
+public:
+	int value;
+
+	Literal(int v) : value{v} {};
+	
+	virtual void print() const override {
+		std::cout << value << std::endl;
+	}
+
+	virtual int eval() const override {
+		return value;
+	}
+};
+
+int main()
+{
+	Literal x {10};
+	x.print();
+	std::cout << "Valor de x = " << x.eval();
+	return 0;
+}
