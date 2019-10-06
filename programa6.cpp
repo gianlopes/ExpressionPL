573d22e5c789b3f8d253319655a11eeab3566554
Gian Lopes <gianlucas1020@hotmail.com>
2019-10-06 18:46:37 -0300

    Add empty line at end of file

--- programa6.cpp
+++ programa6.cpp
@@ -13,7 +13,8 @@ compilar com g++ -o programa6 programa6.cpp lp.o le.o np.o ne.o
 int main()
 {
     /* Instanciando a classe de testes. */
-    TestNegEval<TestLitEval<TestNeg<Test>>> teste;
+    TestNegEval<TestLitEval<TestNeg<TestLit>>> teste;
     /* Lembrando que a ordem importa. */
     teste.run();
+    return 0;
 }
