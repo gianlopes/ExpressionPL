6bc92d5e7c2eede8b87049b39ddf90074f4e18d3
gianlopes <gianlucas1020@hotmail.com>
2019-10-07 09:33:09 -0300

    Update readme and comments

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ExpressionPL
-Parte 2 do trabalho
+This project follows the article ["Evaluating Support for Features in Advanced Modularization Technologies"](https://www.researchgate.net/publication/221496303_Evaluating_Support_for_Features_in_Advanced_Modularization_Technologies) by Lopez-Herrejon, Roberto & Batory, Don & Cook, William. (2005).
--- src/lp.cpp
+++ src/lp.cpp
@@ -1,28 +1,32 @@
 #ifndef LP_H
 #define LP_H
-//modulo lit - print
+
+// Module Lit - print
+
 #include <iostream>
 
-//Interface Exp
+// This module creates interface Exp containing the print method.
 class Exp {
 public:
 	virtual void print(std::ostream &stream = std::cout) const = 0;
 };
 
-//Tipo de dados lit apenas com a funcionalidade print()
-template<typename T = Exp> //O template default é a classe Exp, mas pode-se usar qualquer uma
+// The Lit class that can implement Exp print method
+// and is used to represent an Int literal.
+template<typename T = Exp> // Default template is Exp
 class Lit : public T {
 public:
 	int value;
 
 	Lit(int v) : value{v} {};
 	
+	// Print implemantation, defaults output to std::cout
 	virtual void print(std::ostream &stream = std::cout) const override {
 		stream << value;
 	}
 };
 
-//classe de teste
+// Test class - used in the article to test module compositions
 class TestLit {
 public:
 	Lit<> ltree;
